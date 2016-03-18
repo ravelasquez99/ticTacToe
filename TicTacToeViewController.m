ca350aec8a28494a66b5714c7174aefff16ee502
ravelasquez99 <ricky.wedew@gmail.com>
2016-03-17 19:46:38 -0700

    added method to show an alert controller declaring the winner

--- TicTacToeViewController.m
+++ TicTacToeViewController.m
@@ -21,21 +21,46 @@
 @property (weak, nonatomic) IBOutlet UILabel *currentTurnLabel;
 
 @property (weak, nonatomic) IBOutlet UILabel *winnerOrTieLabel;
+@property BOOL xIsCurrentTurn;
+
 
 @end
 
 @implementation TicTacToeViewController
+NSString *xIsUp = @"X is up!";
+NSString *OIsUp = @"O is up!";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //set current player turn to have x starting
+    self.xIsCurrentTurn = YES;
+    self.currentTurnLabel.text = xIsUp;
+
+}
+
+//created a method to change the player turn
+-(void) changePlayerTurn {
+    if (self.xIsCurrentTurn) {
+        self.xIsCurrentTurn = NO;
+        self.currentTurnLabel.text = OIsUp;
+    } else {
+        self.xIsCurrentTurn = YES;
+        self.currentTurnLabel.text = xIsUp;
+
+    }
+}
+
+-(void)showWinner:(NSString *)winner{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle: [NSString stringWithFormat:@"%@ won the game!", winner] message:nil preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                          handler:^(UIAlertAction * action) {}];
+    [alert addAction:defaultAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
 
     
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 
